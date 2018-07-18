c38c7f2d39d87a97a75d6e662224f6df49a7f251
Cristian Scalia <cristian.scalia@davinci.edu.ar>
2018-07-18 20:28:51 -0300

    Player Input - Game Manager PREFAB

--- Assets/Scripts/Player/Inputs/PlayerInput.cs
+++ Assets/Scripts/Player/Inputs/PlayerInput.cs
@@ -8,4 +8,9 @@ public class PlayerInput {
     public virtual float RotationY { get { return Input.GetAxis("Mouse Y") * Time.deltaTime; } }
     public virtual bool Aiming { get { return Input.GetKey(KeyCode.Mouse1); } }
     public virtual bool Shooting { get { return Input.GetKey(KeyCode.Mouse0); } }
+    public virtual bool Sk1 { get { return Input.GetKey(KeyCode.Alpha1); } }
+    public virtual bool Sk2 { get { return Input.GetKey(KeyCode.Alpha2); } }
+    public virtual bool Sk3 { get { return Input.GetKey(KeyCode.Alpha3); } }
+    public virtual bool Sk4 { get { return Input.GetKey(KeyCode.Alpha4); } }
+
 }
