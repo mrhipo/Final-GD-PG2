5078bd04c6b525698c1f6a124606eaa63bf8a814
Silvestre Queiruga <sylvergq@gmail.com>
2018-07-20 22:51:44 -0300

    Level 2
    Assests para el segundo nivel

---
+++ Assets/Scripts/EventSystem/GameEvents/EnemyKilledEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKilledEvent : GameEvent
+{
+    GameObject _enemy; //cambiar GO por la clase enemies generica
+
+	public EnemyKilledEvent(GameObject enemy)
+    {
+        _enemy = enemy;
+    }
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        GlobalEvent.Instance.AddEventHandler<EnemyKilledEvent>(OnKilledEnemy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnKilledEnemy(GameEvent enemyKilledEvent)
+    {
+      
+    }
+}
