3068845a11d39b8c64c51ba7f7c410c81def4e68
Cristian Scalia <cristian.scalia@davinci.edu.ar>
2018-07-28 11:01:04 -0300

    ToggleCanAttack

--- Assets/Scripts/Enemies/SpecialAttackBehaviour.cs
+++ Assets/Scripts/Enemies/SpecialAttackBehaviour.cs
@@ -25,6 +25,7 @@ public abstract class SpecialAttackBehaviour : MonoBehaviour
         {
             enemyStats.fsm.SetTrigger("SpecialAttack");
             ExecuteSpecialAttack();
+            ToggleCanAttack();
         }
     }
 
