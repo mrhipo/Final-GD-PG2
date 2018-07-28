ca80fff4387f7dddf0782ff5f158786a55f4bbb2
Patricio Pagani Palmbaum <Patriciosk8x@hotmail.com>
2018-07-28 12:07:53 -0300

    Grunt Special Attack (Charge / Dash)

--- Assets/Scripts/NewFsm/GruntSpecial.cs
+++ Assets/Scripts/NewFsm/GruntSpecial.cs
@@ -7,23 +7,45 @@ using UnityEngine;
 
 public class GruntSpecial : SpecialAttackBehaviour
 {
-	public float attackRange;	
+	public float attackRange;
+	public float castDashDelay;
+	
+	private Vector3 _dashPosition;
+	
 	//Cabeceada para testeo
 	public override bool Condition
 	{
 		get {
-            return Vector3.Distance(transform.position, enemyStats.target.position) <= attackRange ;	 
+			return Vector3.Distance(transform.position, enemyStats.target.position) <= attackRange &&
+			       Vector3.Distance(transform.position, _dashPosition) >= enemyStats.agent.stoppingDistance;	
 		}
 	}
 
     public override void ExecuteSpecialAttack()
     {
-	    enemyStats.agent.speed = enemyStats.speed * 2;
-	    
-	    FrameUtil.AfterDelay(4, () => enemyStats.fsm.SetTrigger("SpecialFinish"));
-	    FrameUtil.AfterDelay(4, () => enemyStats.agent.speed = enemyStats.speed);
-         print("//TODO MAGIC");
+	    print("//TODO MAGIC");
+	    enemyStats.agent.isStopped = true;
+		FrameUtil.AfterDelay(castDashDelay, () => Dash());
     }
+
+	private void Dash()
+	{
+		_dashPosition = enemyStats.TargetPosition;
+		enemyStats.agent.speed = enemyStats.speed * 2;
+
+		enemyStats.agent.SetDestination(_dashPosition);
+		enemyStats.agent.isStopped = false;
+	    
+		SpecialFinish();
+		
+		FrameUtil.AfterDelay(4, () => FinishAttack());
+	}
+
+	private void FinishAttack()
+	{
+		enemyStats.agent.speed = enemyStats.speed;
+		Debug.Log("End Magic");
+	}
 }
 
 
