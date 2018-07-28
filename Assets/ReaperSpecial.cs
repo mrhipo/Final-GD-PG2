62bb5e52088fa29de4539162cf31e1b8bc4a6a89
Cristian Scalia <cristian.scalia@davinci.edu.ar>
2018-07-28 09:41:25 -0300

    Reaper

---
+++ Assets/ReaperSpecial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperSpecial : SpecialAttackBehaviour
+{
+
+    //Cabeceada para testeo
+    public bool active;
+
+    //Cabeceada para testeo
+    public override bool Condition
+    {
+        get
+        {
+            var b = (enemyStats.currentState & statesCondition) > 0 && active;
+            if (b)
+            {
+                //Cabeceada para testeo
+                active = false;
+                //Cabeceada para testeo
+                FrameUtil.AfterDelay(8, () => active = true);
+                //Cabeceada para testeo
+                FrameUtil.AfterDelay(4, () => enemyStats.fsm.SetTrigger("SpecialFinish"));
+            }
+            return b;
+        }
+    }
+
+}
