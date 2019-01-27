da1b15b37ab3f0d01a293dcfa4a72e3f40cf10c2
Cristian Scalia <cristian.scalia@davinci.edu.ar>
2019-01-27 01:07:48 -0300

    Enemy Warlock

--- Assets/Scripts/Enemies/RangeAttackBehaviour.cs
+++ Assets/Scripts/Enemies/RangeAttackBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,15 +10,23 @@ public class RangeAttackBehaviour : AttackBehaviour
 
     private EnemyStats _enemyStats;
 
+    event Action OnUpdate = delegate { };
+
     private void Start()
     {
         _enemyStats = GetComponent<EnemyStats>();
+    }
+
+    public void Update()
+    {
+        OnUpdate();
     }
 
     public override void Attack()
     {
         _enemyStats.Speed = 0;
         _enemyStats.agent.isStopped = true;
+        OnUpdate += LookToTarget;
 
         FrameUtil.AfterDelay(delayToActivateAttack, () =>
         {
@@ -29,10 +38,16 @@ public class RangeAttackBehaviour : AttackBehaviour
         {
             _enemyStats.Speed = _enemyStats.speed;
             _enemyStats.agent.isStopped = false;
-            
+            OnUpdate -= LookToTarget;
+
             _enemyStats.fsm.SetTrigger("Idle");
             Debug.Log("End Magic");
         });
         
+    }
+
+    private void LookToTarget()
+    {
+        transform.forward = Vector3.Lerp(transform.forward, _enemyStats.target.position - transform.position, 1f);
     }
 }
