51a2cda961f19baf4d737d635a47f4470e1a24e5
Cristian Scalia <cristian.scalia@davinci.edu.ar>
2019-02-02 16:38:46 -0300

    Fix Doors With missing referenecs With Tools

--- Assets/Scripts/SaveSystem/Editor/EditorSaveObject.cs
+++ Assets/Scripts/SaveSystem/Editor/EditorSaveObject.cs
@@ -34,10 +34,51 @@ public class EditorSaveObject
                 item.id = id++;
                 EditorUtility.SetDirty(item);
             }
+
+            var doors = gameObject.GetComponentsInChildren<Door>(true);
+            if (doors != null)
+            {
+                foreach (var d in doors)
+                {
+                    d.door = d.transform.Find("Main").gameObject;
+                    d.doorUnLocked = Resources.Load<Material>("MAT_DoorMainOpen");
+                    d.doorLocked = Resources.Load<Material>("MAT_DoorMainClose");
+                }
+            }
         }
 
         
         Debug.Log("FIN SAVE ID");
     }
 
+
+
+    [MenuItem("Tools/Fix Doors Missing References")]
+    private static void FixDoorReferences()
+    {
+
+        List<GameObject> rootObjects = new List<GameObject>();
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+
+        for (int i = 0; i < rootObjects.Count; ++i)
+        {
+            GameObject gameObject = rootObjects[i];
+
+            var doors = gameObject.GetComponentsInChildren<Door>(true);
+            if (doors != null)
+            {
+                foreach (var d in doors)
+                {
+                    d.door = d.transform.Find("Main").gameObject;
+                    d.doorUnLocked = Resources.Load<Material>("MAT_DoorMainOpen");
+                    d.doorLocked = Resources.Load<Material>("MAT_DoorMainClose");
+                    EditorUtility.SetDirty(d);
+                }
+            }
+        }
+
+
+        Debug.Log("Doors fixed!");
+    }
 }
