de74b03415dc27926542eb2b2d4f53cdd6375033
mrhipo <33135102+mrhipo@users.noreply.github.com>
2019-02-03 12:18:26 -0300

    Intro completa

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour
+{
+    public List<Sprite> images;
+    public List<string> texts;
+
+    public Image slide;
+    public Image fade;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Slide());
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Slide()
+    {
+        for (int i = 0; i < images.Count; i++)
+        {
+            slide.sprite = images[i];
+            text.text = texts[i];
+            fade.CrossFadeAlpha(0f, 10f, true);
+            yield return new WaitForSeconds(10f);
+            fade.CrossFadeAlpha(1f, 3f, true);
+            yield return new WaitForSeconds(3f);
+        }
+
+        SceneManager.LoadScene(2);  
+    }
+
+}
