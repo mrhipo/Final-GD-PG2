8224bc5337045ba794baab3335a179c554153bab
Patricio Pagani Palmbaum <Patriciosk8x@hotmail.com>
2019-02-11 11:42:09 -0300

    WinManager

--- Assets/Scripts/EventSystem/GameEvents/LevelCompletedEvent.cs
+++ Assets/Scripts/EventSystem/GameEvents/LevelCompletedEvent.cs
@@ -5,5 +5,4 @@ using UnityEngine;
 public class LevelCompletedEvent : GameEvent
 {
     public int level;
-    
 }
---
+++ Assets/Scripts/Managers/WinManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WinManager : MonoBehaviour
+{
+    public LifeObject bossLifeObject;
+    public GameObject winPanel;
+
+    void Start() { bossLifeObject.OnDead += WinGame; }
+
+    private void WinGame()
+    {
+        bossLifeObject.OnDead -= WinGame;
+        winPanel.SetActive(true);
+        GlobalEvent.Instance.Dispatch(new LevelCompletedEvent() { level = 3 });
+    }
+}
